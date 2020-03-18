8946a08a8da69bc7a913e638811f77a9c6930f31
Sergio <arcaymallo@gmail.com>
2020-03-18 03:24:31 +0100

    rest connection with Django for the route

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-tab1',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tab1Page implements OnInit {
 
-  constructor() { }
+  results: Observable<any>;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+  }
+
+  sendRequest() {
+    var origin = (<HTMLInputElement>document.getElementById("origin_input")).value
+    var destination = (<HTMLInputElement>document.getElementById("destination_input")).value
+    this.results = this.http.get("http://localhost:8000/route/"+origin+"/"+destination);
+    this.results.subscribe(res => console.log(res));
   }
 
 }
