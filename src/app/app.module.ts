c433c8c8222f871a73dc4881c4d91fa90874aab3
Pablotria98 <pablocachafeirodiaz@gmail.com>
2020-04-03 03:23:06 +0200

    Login mediante autenticacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { IonicStorageModule } from '@ionic/storage';
 import {
   MatInputModule,
   MatPaginatorModule,
@@ -47,7 +45,8 @@ import { HttpClientModule } from '@angular/common/http';
     MatIconModule,
     MatButtonModule,
     MatCardModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    IonicStorageModule.forRoot()
   ],  providers: [
     StatusBar,
     SplashScreen,
